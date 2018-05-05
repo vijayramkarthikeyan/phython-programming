71acc86e4573a33f8acfe4ad442ed2bd25939d23
vijayramkarthikeyan <37336382+vijayramkarthikeyan@users.noreply.github.com>
2018-05-05 23:44:58 +0530

    Create to find largest number.py

---
+++ to find largest number.py
@@ -0,0 +1,14 @@
+for z in range(0,3):
+  if z == 0:
+    x = int(input("\nEnter the num1:"))
+  if z == 1:
+    y = int(input("\nEnter the num2:"))
+  if z == 2:
+    w = int(input("\nEnter the num3:"))
+    
+    
+def ashwintaj(a,b,c):
+  print max(a,b,c)
+  
+
+ashwintaj(x,y,w)  
