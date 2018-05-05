b8ccca958f084497d5fb10bcd679a68a8a849cb4
vijayramkarthikeyan <37336382+vijayramkarthikeyan@users.noreply.github.com>
2018-05-05 23:58:41 +0530

    Create To count number of digits.py

---
+++ To count number of digits.py
@@ -0,0 +1,18 @@
+Pizza  = int(input("Enter the number:"))
+
+if Pizza < 10:
+	print "1"
+  
+if Pizza > 9 and box < 100:
+	print "2"
+  
+if Pizza > 99 and box < 1000:
+	print "3"
+  
+if Pizza > 999 and box < 10000:
+	print "4"
+  
+if Pizza > 9999 and box < 100000:
+	print "5"
+if Pizza > 9999 and box < 1000000:
+	print "6"
