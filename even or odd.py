1bd77b4dc3996cb7b8ca90ceaea2cb502be33d80
vijayramkarthikeyan <37336382+vijayramkarthikeyan@users.noreply.github.com>
2018-05-05 23:37:27 +0530

    Create even or odd.py

---
+++ even or odd.py
@@ -0,0 +1,7 @@
+char = raw_input("Enter a character: ")
+
+if(char=='A' or char=='a' or char=='O' or char =='o' or char=='U'
+ or char=='u' or char=='E' or char=='e' or char=='I' or char=='i'):
+    print "vowel"
+else:
+    print "Consonant"
