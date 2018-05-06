aaf3c8c9bc77df3147f949ab046f621feb85af4f
vijayramkarthikeyan <37336382+vijayramkarthikeyan@users.noreply.github.com>
2018-05-06 21:43:49 +0530

    Create inter.py

---
+++ inter.py
@@ -0,0 +1,12 @@
+for i in range(0,2):
+  if i == 0:
+    x = int(input("Enter the firnum:"))
+    
+  if i == 1:
+    y = int(input("Enter the secnum:"))
+    
+x = x + 1
+
+for u in range(x,y):
+  if u%2 == 0:
+    print u
